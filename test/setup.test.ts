1a9bd0bedaff6070571908bd38c5bb3a5afe3420
Jason Macgowan <1389531+jasonmacgowan@users.noreply.github.com>
2020-09-12 19:19:38 -0400

    test(setup): refactor test to match rename

--- test/setup.test.ts
+++ test/setup.test.ts
@@ -1,9 +1,9 @@
-import { read, readConfig } from "../src/params";
+import { readParams, readConfig } from "../src/setup";
 
 describe("params", () => {
   describe("#read", () => {
     test("parses yaml with params definition", () => {
-      const params = read();
+      const params = readParams();
 
       expect(params.length).toBe(4);
     });

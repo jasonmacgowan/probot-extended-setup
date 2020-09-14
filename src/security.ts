cbcc43b28bd7c2b18be46203bfab482774517546
Jason Macgowan <1389531+jasonmacgowan@users.noreply.github.com>
2020-09-14 10:00:03 -0400

    feat(security): add token plumbing

---
+++ src/security.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+import path from "path";
+import generatePassword from "password-generator";
+
+const tokenFilename = ".token";
+const tokenPath = path.join(process.cwd(), tokenFilename);
+
+/**
+ * Generate a token used for authentication that is 32
+ * chars in length.
+ */
+export const generateToken = (): string => {
+  return generatePassword(32);
+};
+
+/**
+ * Write a given token to the default tokenfile location
+ */
+export const writeToken = (token: string): void => {
+  fs.writeFileSync(tokenPath, token);
+};
+
+/**
+ * Read the auth token from the default tokenfile location.
+ */
+export const readToken = (): string => {
+  return fs.readFileSync(tokenPath, "utf8");
+};

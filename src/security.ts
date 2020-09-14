a505c7f09b177c8bc805c49520c3fbbfc207f306
Jason Macgowan <1389531+jasonmacgowan@users.noreply.github.com>
2020-09-14 11:44:39 -0400

    feat: subject is required

--- src/security.ts
+++ src/security.ts
@@ -1,3 +1,4 @@
+import { Request, Response, NextFunction } from "express";
 import fs from "fs";
 import path from "path";
 import generatePassword from "password-generator";
@@ -26,4 +27,42 @@ export const writeToken = (token: string): string => {
  */
 export const readToken = (): string => {
   return fs.readFileSync(tokenPath, "utf8");
+};
+
+/**
+ * Express middleware for enforcing authentication with the token
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+export const authn = (req: Request, res: Response, next: NextFunction) => {
+  const allowlist = ["127.0.0.1", "::f", "::ffff:127.0.0.1"];
+
+  if (allowlist.indexOf(req.connection.remoteAddress || "") >= 0) {
+    // allow access from localhost
+    next();
+    return;
+  }
+
+  const tokenExists = fs.existsSync(tokenPath);
+
+  if (tokenExists) {
+    const token = readToken();
+
+    if (req.session && token === req.session.token) {
+      // token in session matches
+      next();
+      return;
+    } else {
+      req.session?.destroy(function () {
+        res.redirect("/probot/setup/extended/authn");
+      });
+
+      return;
+    }
+  }
+
+  res.writeHead(404);
+  res.end();
 };
